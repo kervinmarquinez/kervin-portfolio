f6ede80977a05930fa35ef87cf7fedbb9c10ef98
Adrián Kervin Marquinez <kervinmarquinez@gmail.com>
2023-02-06 16:19:46 +0100

    download button fixed

--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -5,6 +5,6 @@ export const Download = ({value}) => {
     const {status} = useSelector(state => state.ui)
 
     return (
-        <a href={status ? "https://docs.google.com/document/d/14cSgmGZ80FJnQyX1hUIn3jRBLApnzQXYF3ijfANMnAU/edit?usp=sharing" : "https://docs.google.com/document/d/1z52aaMKU4CFytG1PgXVK6iT7Hr4FESjFnBFf9QmoqgI/edit?usp=sharing"} target="_blank" className='uppercase font-medium tracking-wider border-solid border border-white px-5 py-2 hover:text-xl duration-300'>{value}</a>
+        <a href={status ? "../assets/cv/en.pdf" : "../assets/cv/es.pdf"} download className='uppercase font-medium tracking-wider border-solid border border-white px-5 py-2 hover:text-xl duration-300'>{value}</a>
     )
 }
